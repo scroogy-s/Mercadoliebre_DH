b7569d6262e595018ae354e2e4fd7c11305ae7a7
Francisco Polzoni <francisco.polzoni@gpsde.local>
2023-10-30 15:42:10 -0300

    Quitamos la carpeta src.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, 'views/home.html'))
 });
 
 const port = 3000
