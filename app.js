f5224f8e4555a8846e71ced8f0022fa3e40c6da2
scroogy-s <narufran@yahoo.com>
2023-11-07 12:21:02 -0300

    Ejercitacion 21 agregada, falta comprobaciones

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/',(req,res) => {
 app.get('/register',(req,res) => {
     res.sendFile(path.join(__dirname, 'views/register.html'))
 });
+app.get('/login',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+});
 
 const port = 3000
 app.listen(port , () => {
